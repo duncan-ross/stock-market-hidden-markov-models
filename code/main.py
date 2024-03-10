6b38d5786f23905f160b10911e3b275ada249fb9
Ethan Allavarpu <63200263+ethan-allavarpu@users.noreply.github.com>
2024-03-10 15:34:05 -0700

    update main

--- code/main.py
+++ code/main.py
@@ -1,28 +1,61 @@
-import pandas as pd
 from constants import train_trials
 from functions import load_data, run_trial
+import pandas as pd
 
 if __name__ == "__main__":
     dfs = load_data(tcks=("AAPL", "DELL", "FORD", "IBM", "MACYS", "SP500"))
 
     # Paper constants (adjust as needed)
-    latency = [6, 10, 20, 50]
-    n_states = [3, 4, 5, 6]
+    additional_latency = [6, 20, 50]
+    additional_n_states = [3, 5, 6, 8]
 
     for tck, trials in train_trials.items():
         preds = pd.DataFrame()
         for i, trial in trials.items():
-            for lat in latency:
-                for n in n_states:
+            if tck == "AAPL" and i == 1:
+                for lat in additional_latency:
                     results, preds_df = run_trial(
                         dfs,
-                        tck=tck, train_period=trial["train"], test_period=trial["test"],
-                        latency=lat, n_states=n
+                        tck=tck,
+                        train_period=trial["train"],
+                        test_period=trial["test"],
+                        latency=lat,
+                        n_states=4,
                     )
                     preds_df["experiment_id"] = i
                     preds_df["min_train_date"] = trial["train"][0]
                     preds_df["max_train_date"] = trial["train"][1]
                     preds_df["latency"] = lat
+                    preds_df["n_states"] = 4
+                    preds = pd.concat([preds, preds_df])
+                for n in additional_n_states:
+                    results, preds_df = run_trial(
+                        dfs,
+                        tck=tck,
+                        train_period=trial["train"],
+                        test_period=trial["test"],
+                        latency=10,
+                        n_states=n,
+                    )
+                    preds_df["experiment_id"] = i
+                    preds_df["min_train_date"] = trial["train"][0]
+                    preds_df["max_train_date"] = trial["train"][1]
+                    preds_df["latency"] = 10
                     preds_df["n_states"] = n
                     preds = pd.concat([preds, preds_df])
+            results, preds_df = run_trial(
+                dfs,
+                tck=tck,
+                train_period=trial["train"],
+                test_period=trial["test"],
+                latency=10,
+                n_states=4,
+            )
+            preds_df["experiment_id"] = i
+            preds_df["min_train_date"] = trial["train"][0]
+            preds_df["max_train_date"] = trial["train"][1]
+            preds_df["latency"] = 10
+            preds_df["n_states"] = 4
+            preds = pd.concat([preds, preds_df])
+
         preds.to_csv(f"../predictions/{tck}/predictions.csv", index=False)
